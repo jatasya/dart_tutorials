f1d62a81f8d2e9226145428c7b6cd7a392e2cac7
vikas-nichi <vikas.ep@nichi.com>
2020-04-05 12:40:45 +0530

    data types

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+main() {
+  int num1 = 1;
+  var num2 = 2;
+
+  print('$num1, $num2 \n');
+
+   double dnum1 = 1.10;
+  var dnum2 = 2.30;
+
+  print('$dnum1, $dnum2 \n');
+
+   String s1 = 'Vikas';
+  var s2 = 'E P';
+
+  print('$s1, $s2 \n');
+
+   bool t1 = true;
+  var f1 = false;
+
+  print('$t1, $f1 \n');
+
+   dynamic d1 = "Cool";
+  var d2 = 234234;
+
+  print('$d1, $d2 \n');
+
+}

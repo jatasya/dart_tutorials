49039d375dbac92cb5f4d73e1c86992e41ac789b
vikas-nichi <vikas.ep@nichi.com>
2020-04-05 11:55:44 +0530

    static type and dynamic inferencing

--- playground.dart
+++ playground.dart
@@ -1,3 +1,5 @@
 main() {
-  print('hello world');
+  var firstName = 'Vikas'; // can accept any value
+  String lastName = "E P"; // can accept only string values
+  print(firstName + ' ' + lastName);
 }

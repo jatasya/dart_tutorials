dc055ef403b246c728b725038569dadd565d2200
vikas-nichi <vikas.ep@nichi.com>
2020-04-05 12:59:55 +0530

    strings

---
+++ Strings_delimiter.dart
@@ -0,0 +1,16 @@
+main() {
+  var s1 = 'Single quotes are good for string literals.';
+  var s2 = "Double quotes work good too.";
+  var s3 = 'It\'s easy to excape string deliminter';
+  var s4 = "It's Even easier with double quotes";
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print('object');
+// r for raw
+  var s5 = r'with raw even \n is not excluded';
+
+  print(s5);
+}
